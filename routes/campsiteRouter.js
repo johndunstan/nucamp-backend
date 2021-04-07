252bc40a4528b96b9894ac3ac581e06c7977b8e3
John Dunstan <johntdunstan@gmail.com>
2021-04-07 10:39:01 -0600

    Move campsite routes to own file

---
+++ routes/campsiteRouter.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const campsiteRouter = express.Router()
+
+campsiteRouter
+  .route('/')
+  .all((req, res, next) => {
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/plain')
+    next()
+  })
+  .get((req, res) => {
+    res.end('Will send all the campsites to you')
+  })
+  .post((req, res) => {
+    res.end(
+      `Will add the campsites: ${req.body.name} with description ${req.body.description}`
+    )
+  })
+  .put((req, res) => {
+    res.statusCode = 403
+    res.end('PUT operation not supported on /campsites')
+  })
+  .delete((req, res) => {
+    res.end('Deleting all campsites')
+  })
+
+module.exports = campsiteRouter
